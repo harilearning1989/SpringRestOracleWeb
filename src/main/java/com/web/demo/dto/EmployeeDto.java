804505d2a909546264aefd045da9f89a6ec6bbc5
Hari <hari.learning1989@gmail.com>
2021-05-20 08:03:24 -0700

    Removed un used classes

---
+++ src/main/java/com/web/demo/dto/EmployeeDto.java
@@ -0,0 +1,269 @@
+package com.web.demo.dto;
+
+import java.util.Date;
+
+public class EmployeeDto {
+
+	private int id;
+	private int empID;
+	private String namePrefix;
+	private String firstName;
+	private String middleInitial;
+	private String lastName;
+	private String gender;
+	private String eMail;
+	private String fathersName;
+	private String mothersName;
+	private String mothersMaidenName;
+	private Date dateofBirth;
+	private Date timeofBirth;
+	private double ageinYrs;
+	private int weightinKgs;
+	private Date dateofJoining;
+	private String quarterofJoining;
+	private String halfofJoining;
+	private String yearofJoining;
+	private int monthofJoining;
+	private String monthNameofJoining;
+	private String shortMonth;
+	private int dayofJoining;
+	private String dOWofJoining;
+	private String shortDOW;
+	double ageinCompanyYears;
+	private int salary;
+	private int lastHike;
+	private String sSN;
+	private String phoneNo;
+	private String placeName;
+	private String county;
+	private String city;
+	private String state;
+	private int zip;
+	private String region;
+	private String userName;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getEmpID() {
+		return empID;
+	}
+	public void setEmpID(int empID) {
+		this.empID = empID;
+	}
+	public String getNamePrefix() {
+		return namePrefix;
+	}
+	public void setNamePrefix(String namePrefix) {
+		this.namePrefix = namePrefix;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getMiddleInitial() {
+		return middleInitial;
+	}
+	public void setMiddleInitial(String middleInitial) {
+		this.middleInitial = middleInitial;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String geteMail() {
+		return eMail;
+	}
+	public void seteMail(String eMail) {
+		this.eMail = eMail;
+	}
+	public String getFathersName() {
+		return fathersName;
+	}
+	public void setFathersName(String fathersName) {
+		this.fathersName = fathersName;
+	}
+	public String getMothersName() {
+		return mothersName;
+	}
+	public void setMothersName(String mothersName) {
+		this.mothersName = mothersName;
+	}
+	public String getMothersMaidenName() {
+		return mothersMaidenName;
+	}
+	public void setMothersMaidenName(String mothersMaidenName) {
+		this.mothersMaidenName = mothersMaidenName;
+	}
+	public Date getDateofBirth() {
+		return dateofBirth;
+	}
+	public void setDateofBirth(Date dateofBirth) {
+		this.dateofBirth = dateofBirth;
+	}
+	public Date getTimeofBirth() {
+		return timeofBirth;
+	}
+	public void setTimeofBirth(Date timeofBirth) {
+		this.timeofBirth = timeofBirth;
+	}
+	public double getAgeinYrs() {
+		return ageinYrs;
+	}
+	public void setAgeinYrs(double ageinYrs) {
+		this.ageinYrs = ageinYrs;
+	}
+	public int getWeightinKgs() {
+		return weightinKgs;
+	}
+	public void setWeightinKgs(int weightinKgs) {
+		this.weightinKgs = weightinKgs;
+	}
+	public Date getDateofJoining() {
+		return dateofJoining;
+	}
+	public void setDateofJoining(Date dateofJoining) {
+		this.dateofJoining = dateofJoining;
+	}
+	public String getQuarterofJoining() {
+		return quarterofJoining;
+	}
+	public void setQuarterofJoining(String quarterofJoining) {
+		this.quarterofJoining = quarterofJoining;
+	}
+	public String getHalfofJoining() {
+		return halfofJoining;
+	}
+	public void setHalfofJoining(String halfofJoining) {
+		this.halfofJoining = halfofJoining;
+	}
+	public String getYearofJoining() {
+		return yearofJoining;
+	}
+	public void setYearofJoining(String yearofJoining) {
+		this.yearofJoining = yearofJoining;
+	}
+	public int getMonthofJoining() {
+		return monthofJoining;
+	}
+	public void setMonthofJoining(int monthofJoining) {
+		this.monthofJoining = monthofJoining;
+	}
+	public String getMonthNameofJoining() {
+		return monthNameofJoining;
+	}
+	public void setMonthNameofJoining(String monthNameofJoining) {
+		this.monthNameofJoining = monthNameofJoining;
+	}
+	public String getShortMonth() {
+		return shortMonth;
+	}
+	public void setShortMonth(String shortMonth) {
+		this.shortMonth = shortMonth;
+	}
+	public int getDayofJoining() {
+		return dayofJoining;
+	}
+	public void setDayofJoining(int dayofJoining) {
+		this.dayofJoining = dayofJoining;
+	}
+	public String getdOWofJoining() {
+		return dOWofJoining;
+	}
+	public void setdOWofJoining(String dOWofJoining) {
+		this.dOWofJoining = dOWofJoining;
+	}
+	public String getShortDOW() {
+		return shortDOW;
+	}
+	public void setShortDOW(String shortDOW) {
+		this.shortDOW = shortDOW;
+	}
+	public double getAgeinCompanyYears() {
+		return ageinCompanyYears;
+	}
+	public void setAgeinCompanyYears(double ageinCompanyYears) {
+		this.ageinCompanyYears = ageinCompanyYears;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	public int getLastHike() {
+		return lastHike;
+	}
+	public void setLastHike(int lastHike) {
+		this.lastHike = lastHike;
+	}
+	public String getsSN() {
+		return sSN;
+	}
+	public void setsSN(String sSN) {
+		this.sSN = sSN;
+	}
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+	public String getPlaceName() {
+		return placeName;
+	}
+	public void setPlaceName(String placeName) {
+		this.placeName = placeName;
+	}
+	public String getCounty() {
+		return county;
+	}
+	public void setCounty(String county) {
+		this.county = county;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public int getZip() {
+		return zip;
+	}
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+	public String getRegion() {
+		return region;
+	}
+	public void setRegion(String region) {
+		this.region = region;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	
+
+}
